ce9ac48b8159855e210bb6728e70ff050cf2d18e
AshleighWong <47904190+AshleighWong@users.noreply.github.com>
2023-03-10 03:49:11 -0500

    added the tests

--- tests.cpp
+++ tests.cpp
@@ -1,5 +1,29 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
-#include "funcs.h"
+#include "caesar.h"
+#include "vigenere.h"
+#include "decrypt.h"
+#include <iostream>
+#include <string>
+#include <cctype>
 
-// add your tests here
+TEST_CASE("Task B: Caesar")
+{
+    CHECK(encryptCaesar("Way to Go!", 5) == "Bfd yt Lt!");
+}
+
+TEST_CASE("Task C: Vigenere")
+{
+    CHECK(encryptVigenere("Hello, World!", "cake") == "Jevpq, Wyvnd!");
+}
+
+TEST_CASE("Task D: Decrypt Caeser")
+{
+    CHECK(decryptCaesar("Rovvy, Gybvn!", 10) == "Hello, World!");
+    CHECK(decryptCaesar("Bfd yt Lt!", 5) == "Way to Go!");
+}
+
+TEST_CASE("Task D: Decrypt Vigenere")
+{
+    CHECK(decryptVigenere("Jevpq, Wyvnd!", "cake") == "Hello, World!");
+}
