ddb6917a0e318a773f28b81daa3089925fdba0f4
AshleighWong <47904190+AshleighWong@users.noreply.github.com>
2023-03-10 03:50:20 -0500

    added main tests

--- main.cpp
+++ main.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
-
-
-#include "funcs.h"
+#include "caesar.h"
+#include "vigenere.h"
+#include "decrypt.h"
 
 int main()
-{
+{  
+  std::cout << encryptCaesar("A Light-Year Apart", 5) << "\n";
+  std::cout << encryptCaesar("Hello, World!", 10) << "\n";
+
+  std::cout << encryptVigenere("Hello, World!", "cake") << "\n";
+  
+  std::cout << decryptCaesar("Rovvy, Gybvn!", 10) << "\n";
+  std::cout << decryptVigenere("Jevpq, Wyvnd!", "cake") << "\n";
+  
+  std::cout << decryptCaesar("F Qnlmy-Djfw Fufwy", 5) << "\n";
+  std::cout << decryptVigenere("Jevpq, Wyvnd!", "CAKE") << "\n";
+  
+  std::cout << "\n";
+
+
   return 0;
-}
+ }
